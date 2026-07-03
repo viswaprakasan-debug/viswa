a3151cbc1da688f08604f6fe8b677b9791a6e1f3
viswaprakasan <viswaprakasan@gmail.com>
2026-07-03 14:03:41 +0530

    push

---
+++ script.js
@@ -0,0 +1,85 @@
+// console.log("Hello, World!");
+
+// // let, var, const
+
+// var name = "John";
+// console.log(name); // Output: John
+// var name = "Doe"; // This will cause an error because 'name' is already declared with 'let'
+// console.log(name); // Output: John
+
+// let age = 30;
+// console.log(age); // Output: 30
+//  age =31;
+// console.log(age); // Output: 30
+
+// const country = "USA";
+// // country = "Canada"; // This will cause an error because 'country' is a constant
+// console.log(country); // Output: USA
+
+
+// //  +. _ *, /, % 
+
+// let a = 10;
+// let b = 3;
+// console.log(a + b);
+// let c = a * b;
+// console.log(`a : ${a} b : ${b} c : ${c}`);
+
+
+// name = "John";
+// age = 30;
+// male = true;
+// var city;
+// city = "New York";
+// console.log(city)
+// var country = null;
+// var hobbies = ["reading", "traveling", "cooking"];
+// const person = {
+//     name: "John",
+//     age: 30,
+// }
+// console.log(person.name); // Output: John
+// console.log(person.age); // Output: 30
+// person.name = "Doe"; // This is allowed because we are modifying the property of the object, not reassigning the object itself
+// console.log(person.name); // Output: Doe
+
+
+// operators aithmatic, comparison, logical, assignment, 
+
+// age = 30;
+// if (age >= 18) {
+//     console.log("You are an adult.");
+// }else if (age < 18 && age >= 13) {
+//     console.log("You are a teenager.");
+// }else{
+//     console.log("You are not an adult or teenager.");
+// }
+
+// age=10;
+// if (age>=18) {
+//     console.log("You are an adult.");
+// } else if (age<18 && age>=15) {
+//     console.log("You are a teenager.");
+// } else {
+//     console.log("You are not an adult or teenager.");
+// }
+
+
+// var hobbies = ["reading", "traveling", "cooking"];
+// const person = {
+//     name: "navin",
+//     age: 30,
+// }
+// console.log(person.name); 
+// console.log(person.age);
+// person.name = "Doe";
+// console.log(person.name); 
+
+var car=['audi','bmw','mercedes'];
+const person = {
+    name: "praveen"
+}
+console.log(person.name);
+console.log(car[0]);
+person.name="navin";
+console.log(person.name);
